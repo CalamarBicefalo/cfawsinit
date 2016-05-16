5150dc0c15a02797529c93aafa26fb4a1e04f64c
datianshi <dsz0111@gmail.com>
2016-05-15 21:38:52 -0500

    Add internal dns records

--- dnsmapping.py
+++ dnsmapping.py
@@ -15,46 +15,47 @@ def map_ert_domain(stackname, domain, lbname=None,
     if not domain.endswith('.'):
         domain += '.'
 
-    zone = next(
-        (z for z in route53.list_hosted_zones()['HostedZones']
-         if domain.endswith(z['Name'])), None)
+    zones = [z for z in route53.list_hosted_zones()['HostedZones'] if domain.endswith(z['Name'])]
+    for zone in zones:
+        # based on standard naming
+        if zone['Config']['PrivateZone'] == False:
+            lbname = stackname + "-pcf-elb"
+        if zone['Config']['PrivateZone'] == True:
+            lbname = stackname + "-pcf-elb-in"
+        resp = elb.describe_load_balancers(
+            LoadBalancerNames=[lbname])
 
-    # based on standard naming
-    lbname = lbname or stackname + "-pcf-elb"
-    resp = elb.describe_load_balancers(
-        LoadBalancerNames=[lbname])
+        if len(resp.get('LoadBalancerDescriptions', [])) == 0:
+            raise Exception(lbname + " Loadbalacer could not be found")
 
-    if len(resp.get('LoadBalancerDescriptions', [])) == 0:
-        raise Exception(lbname + " Loadbalacer could not be found")
+        dnsname = resp['LoadBalancerDescriptions'][0]['DNSName']
 
-    dnsname = resp['LoadBalancerDescriptions'][0]['DNSName']
+        if not zones:
+            print domain + " Is not managed in route53"
+            print "Manually map {} <-- {}".format(dnsname, names)
+            return
 
-    if zone is None:
-        print domain + " Is not managed in route53"
-        print "Manually map {} <-- {}".format(dnsname, names)
-        return
+        # ensure names are unique
+        names = set(names)
 
-    # ensure names are unique
-    names = set(names)
+        changes = [
+            {
+                'Action': 'UPSERT',
+                'ResourceRecordSet': {
+                    'Name': name,
+                    'Type': 'CNAME',
+                    'TTL': 300,
+                    'ResourceRecords': [
+                        {
+                            'Value': dnsname
+                        },
+                    ],
+                }
+            } for name in names]
 
-    changes = [
-        {
-            'Action': 'UPSERT',
-            'ResourceRecordSet': {
-                'Name': name,
-                'Type': 'CNAME',
-                'TTL': 300,
-                'ResourceRecords': [
-                    {
-                        'Value': dnsname
-                    },
-                ],
-            }
-        } for name in names]
-
-    route53.change_resource_record_sets(
-        HostedZoneId=zone['Id'],
-        ChangeBatch={
-            'Comment': "for stack="+stackname,
-            'Changes': changes
-        })
+        route53.change_resource_record_sets(
+            HostedZoneId=zone['Id'],
+            ChangeBatch={
+                'Comment': "for stack="+stackname,
+                'Changes': changes
+            })
